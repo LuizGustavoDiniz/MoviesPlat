7d12c958bdf1fe621c3fa27403dbdf53b64d18b0
luizgustavodiniz <gustavo.diniz.gon@gmail.com>
2022-08-04 08:46:12 -0300

    1 rota de séries nova

--- controllers/series/allSeriesController.js
+++ controllers/series/allSeriesController.js
@@ -27,4 +27,29 @@ router.get('/series', async (req, res) => {
     }
 })
 
+router.get('/series/best', async (req, res) => {
+
+    try{
+
+        const {data} = await api.get('tv/popular', {
+            params: {
+                api_key: '52c666c7bba4767d261680869bdc65e5',
+                language: 'pt-BR',
+                page: 1
+            }
+         })
+       
+
+         //console.log(data)
+         res.render('series/index', {
+            series: data.results
+         })
+
+         //res.json(data.results)
+
+    }catch(error){
+        res.json({error: error})
+    }
+})
+
 module.exports = router
